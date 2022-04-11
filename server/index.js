7f774bfe30c23eafe848707d6621c8b78e3bb525
ChristinaL24 <christinaah02@gmail.com>
2022-04-11 02:38:44 +0000

    Revised css code on sample one page

--- server/index.js
+++ server/index.js
@@ -5,6 +5,23 @@ const staticMiddleware = require('./static-middleware');
 
 const app = express();
 
+const petfinder = require('@petfinder/petfinder-js');
+const client = new petfinder.Client({ apiKey: '', secret: '' });
+
+client.animal.search({
+  type: 'Dog',
+  limit: 1,
+  location: '92683',
+  distance: 10
+})
+  .then(function (response) {
+    // eslint-disable-next-line no-console
+    console.log(response.data.animals); // Do something with `response.data.animals`
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
 app.use(staticMiddleware);
 
 app.use(errorMiddleware);
