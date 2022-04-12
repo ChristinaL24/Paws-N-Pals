7524010341a46911b19c6f7e3bbdbb0e62227267
ChristinaL24 <christinaah02@gmail.com>
2022-04-12 00:49:59 +0000

    Created first and second feature with css bootstrap

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,28 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."favorites" (
+	"petId" integer NOT NULL,
+	"userId" integer NOT NULL,
+	"details" json NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.users" (
+	"userId" serial NOT NULL,
+	"username" TEXT NOT NULL UNIQUE,
+	"password" TEXT NOT NULL UNIQUE,
+	"joinedAt" timestamp with time zone NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+ALTER TABLE "favorites" ADD CONSTRAINT "favorites_fk0" FOREIGN KEY ("petId") REFERENCES "pets"("petId");
+ALTER TABLE "favorites" ADD CONSTRAINT "favorites_fk1" FOREIGN KEY ("userId") REFERENCES "users"("userId");
