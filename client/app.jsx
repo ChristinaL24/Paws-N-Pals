0bec6567c0870045a570eea6e8b09255aa72192a
ChristinaL24 <christinaah02@gmail.com>
2022-04-14 04:12:19 +0000

    Created card component for matched page

--- client/app.jsx
+++ client/app.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Home from './pages/home';
 import Navbar from './components/navbar';
+import PageContainer from './components/page-container';
 
 export default class App extends React.Component {
+
   render() {
     return (
       <>
         <Navbar />
+        <PageContainer>
         <Home />
+        </PageContainer>
       </>
     );
   }
 }
+
+/*
+
+*/
