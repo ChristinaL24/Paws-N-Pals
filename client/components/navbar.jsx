a95d79266e9ea9856762c010565a55056f5d3c49
ChristinaL24 <christinaah02@gmail.com>
2022-04-19 01:59:33 +0000

    Added the drawer icon to navbar

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default function Navbar(props) {
   return (
       <header>
-        <nav className="navbar navbar-expand-lg green-bg navbar-light bg-light p-0">
+        <nav className="d-flex navbar-expand-lg green-bg navbar-light bg-light p-0">
+        <button className='green-bg'>
+          <i className='fa fa-bars fs-3 text-white'></i>
+        </button>
           <div className="container-fluid green-bg p-1 d-flex justify-content-center">
             <a className="navbar-brand text-white fs-4">Paws N Pals</a>
             <a href='#'>
