3b0624b6b02b998b228948b4b16a0205eb2eaa34
ChristinaL24 <christinaah02@gmail.com>
2022-04-20 05:36:28 +0000

    Issue 4 user can view saved matches

--- client/pages/saved-matches.jsx
+++ client/pages/saved-matches.jsx
@@ -24,27 +24,23 @@ export default class SavedMatches extends React.Component {
 
   render() {
     return (
-      <>
+      <div className="row row-cols-1 row-cols-md-3 g-4 m-5">
         {this.state.animals.map(animal => {
           return (
-            <div key={animal.petId} className="row row-cols-1 row-cols-md-3 g-4 m-5">
-              <div className="col">
-                <div className="card h-100">
-                  <img src={animal.details.photos} className="card-img-top" alt="matched pet" />
-                  <div className="card-body tan-bg">
-                    <h2 className="card-title green-text mb-3">{animal.details.name}</h2>
-                    <p className="card-text text-secondary"><span className="fw-bolder">Location: </span>{animal.details.location}</p>
-                    <p className="card-text text-secondary"><span className="fw-bolder">Age: </span>{animal.details.age}</p>
-                    <p className="card-text text-secondary"><span className="fw-bolder">Breed: </span>{animal.details.breed}</p>
-                  </div>
+            <div key={animal.petId} className='col'>
+              <div className="card h-100">
+                <img src={animal.details.photos} className="card-img-top" alt="matched pet" />
+                <div className="card-body tan-bg">
+                  <h2 className="card-title green-text mb-3">{animal.details.name}</h2>
+                  <p className="card-text text-secondary"><span className="fw-bolder">Location: </span>{animal.details.location}</p>
+                  <p className="card-text text-secondary"><span className="fw-bolder">Age: </span>{animal.details.age}</p>
+                  <p className="card-text text-secondary"><span className="fw-bolder">Breed: </span>{animal.details.breed}</p>
                 </div>
               </div>
             </div>
           );
         })}
-
-      </>
-
+      </div>
     );
   }
 }
