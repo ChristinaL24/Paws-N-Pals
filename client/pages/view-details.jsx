838c75d869277ebfa883233839e9158d7597f45c
ChristinaL24 <christinaah02@gmail.com>
2022-04-24 22:03:04 +0000

    Revised card styling for desktop view

--- client/pages/view-details.jsx
+++ client/pages/view-details.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import GoogleMaps from '../components/google-maps';
 import Geocode from 'react-geocode';
 
+const styles = {
+  image: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'contain'
+  }
+};
+
 export default class ViewDetails extends React.Component {
   constructor(props) {
     super(props);
@@ -42,40 +50,42 @@ export default class ViewDetails extends React.Component {
 
     const { details } = this.state.animal;
     return (
-        <div className='card card-margin'>
-          <div className="row g-0">
-            <div className="col-md-4 tan-bg">
-              <img src={details.photos} className="img-fluid rounded-start" alt={details.name} />
+      <div className="card shadow-sm">
+        <div className="card-body">
+          <div className="row mb-4">
+            <div className="col-12 col-sm-6 col-md-5">
+              <img className='pt-5' src={details.photos} alt={details.name} style={styles.image} />
             </div>
-            <div className="col-md-8 tan-bg">
-              <div className="card-body p-4">
-                <h2 className="card-title green-text mb-4 media-font-size d-flex justify-content-center">{details.name}</h2>
-                  <p className="card-text text-secondary"><span className="fw-bolder">Address: </span>{details.address.city}, {details.address.state}</p>
-                  <p className="card-text text-secondary"><span className="fw-bolder">Age: </span>{details.age}</p>
-                  <p className="card-text text-secondary"><span className="fw-bolder">Breed: </span>{details.breed}</p>
-                  <p className="card-text text-secondary"><span className="fw-bolder">Gender: </span>{details.gender} </p>
-                  <p className="card-text text-secondary"><span className="fw-bolder">Size: </span>{details.size} </p>
-                  <p className="card-text text-secondary">
-                    <span className="fw-bolder">URL: </span>
-                    <a href={details.url} className='text-secondary'>{details.url}</a>
-                  </p>
-                  <p className="card-text text-secondary">
-                    <span className="fw-bolder">Location: </span>
-                  </p>
-                  <GoogleMaps coordinates={this.state.map}/>
-                  <p className='card-text text-secondary'>
-                    {details.address.address1} <br/>
-                    {details.address.city}, {details.address.state}, {details.address.postcode}
-                  </p>
-                  <p className="card-text text-secondary"><span className="fw-bolder">Email: </span>{details.email}</p>
-                  <p className="card-text text-secondary"><span className="fw-bolder">Phone: </span>{details.phone}</p>
-              </div>
+            <div className="col-12 col-sm-6 col-md-7">
+              <h2 className="green-text m-3 d-flex justify-content-center">{details.name}</h2>
+              <p className="text-secondary"><span className="fw-bolder">Located In: </span>{details.address.city}, {details.address.state}</p>
+              <p className="text-secondary"><span className="fw-bolder">Age: </span>{details.age}</p>
+              <p className="text-secondary"><span className="fw-bolder">Breed: </span>{details.breed}</p>
+              <p className="text-secondary"><span className="fw-bolder">Gender: </span>{details.gender} </p>
+              <p className="text-secondary"><span className="fw-bolder">Size: </span>{details.size} </p>
+              <p className="text-secondary">
+                <span className="fw-bolder">URL: </span>
+                <a href={details.url} className='text-secondary'>{details.url}</a>
+              </p>
+            </div>
+            <div>
+              <p className="text-secondary">
+                <span className="fw-bolder">Pal&#39;s Address: </span>
+              </p>
+              <GoogleMaps coordinates={this.state.map} />
+              <p className='text-secondary'>
+                {details.address.address1} <br />
+                {details.address.city}, {details.address.state}, {details.address.postcode}
+              </p>
+              <p className="text-secondary"><span className="fw-bolder">Email: </span>{details.email}</p>
+              <p className="text-secondary"><span className="fw-bolder">Phone: </span>{details.phone}</p>
               <a href='#saved-matches'>
-                <i className="fa-solid fa-circle-arrow-left green-text ps-4 pb-3"></i>
+                <i className="fa-solid fa-circle-arrow-left green-text pb-3"></i>
               </a>
             </div>
           </div>
         </div>
+      </div>
     );
   }
 }
